cb8fa2713e977d23e11bcafcc780d10670882d78
mrkaragoz <mrkaragoz@yahoo.com>
2023-05-17 15:42:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 import os
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 @app.route('/')
+@cross_origin()
 def index():
     return "<span>He he :)</span>"
 
